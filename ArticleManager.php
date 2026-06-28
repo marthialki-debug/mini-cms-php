bffc5e9fe5e6cad566c69b90926f3f3d2f909a62
marthialki-debug <marthialki@gmail.com>
2026-06-28 20:26:25 +0000

    Retrait temporaire de modifierArticle pour reattribution

--- ArticleManager.php
+++ ArticleManager.php
@@ -12,13 +12,6 @@ class ArticleManager
         ];
     }
 
-    public function modifierArticle($id, $donnees)
-    {
-        if (isset($this->articles[$id])) {
-            $this->articles[$id] = array_merge($this->articles[$id], $donnees);
-        }
-    }
-
     public function supprimerArticle($id)
     {
         unset($this->articles[$id]);
